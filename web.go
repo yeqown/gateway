298eb1aa7f6d8c00c6e1d58d196cb7c5d3eb594e
yeqown <yeqown@gmail.com>
2018-12-14 18:55:26 +0800

    del web page files

--- web.go
+++ web.go
@@ -1,56 +1,56 @@
 package gateway
 
-import (
-	"fmt"
-	"html/template"
-	"net/http"
-	"path"
-	"strings"
+// import (
+// 	"fmt"
+// 	"html/template"
+// 	"net/http"
+// 	"path"
+// 	"strings"
 
-	"github.com/yeqown/gateway/logger"
-)
+// 	"github.com/yeqown/gateway/logger"
+// )
 
-// Prefix of URL
-const webPrefix = "/html/"
+// // Prefix of URL
+// const webPrefix = "/html/"
 
-// HTMLSrv ...
-var HTMLSrv *HTMLServer
+// // HTMLSrv ...
+// var HTMLSrv *HTMLServer
 
-func init() {
-	mux := http.NewServeMux()
+// func init() {
+// 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", IndexPage)
-	mux.HandleFunc("/p1", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "page here!")
-		return
-	})
-	HTMLSrv = &HTMLServer{
-		mux:      mux,
-		HTMLRoot: "../../web/html",
-	}
-}
+// 	mux.HandleFunc("/", IndexPage)
+// 	mux.HandleFunc("/p1", func(w http.ResponseWriter, req *http.Request) {
+// 		fmt.Fprintf(w, "page here!")
+// 		return
+// 	})
+// 	HTMLSrv = &HTMLServer{
+// 		mux:      mux,
+// 		HTMLRoot: "../../web/html",
+// 	}
+// }
 
-// HTMLServer ...
-type HTMLServer struct {
-	mux      *http.ServeMux
-	HTMLRoot string
-}
+// // HTMLServer ...
+// type HTMLServer struct {
+// 	mux      *http.ServeMux
+// 	HTMLRoot string
+// }
 
-// ServeHTTP ...
-func (s *HTMLServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	logger.Logger.Info(req.URL.Path)
+// // ServeHTTP ...
+// func (s *HTMLServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+// 	logger.Logger.Info(req.URL.Path)
 
-	req.URL.Path = strings.TrimPrefix(req.URL.Path, webPrefix)
-	req.URL.Path = "/" + req.URL.Path
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+// 	req.URL.Path = strings.TrimPrefix(req.URL.Path, webPrefix)
+// 	req.URL.Path = "/" + req.URL.Path
+// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	s.mux.ServeHTTP(w, req)
-}
+// 	s.mux.ServeHTTP(w, req)
+// }
 
-// IndexPage ...
-func IndexPage(w http.ResponseWriter, req *http.Request) {
-	fp := path.Join(HTMLSrv.HTMLRoot, "index.html")
-	tmpl := template.Must(template.ParseFiles(fp))
-	tmpl.Execute(w, nil)
-	return
-}
+// // IndexPage ...
+// func IndexPage(w http.ResponseWriter, req *http.Request) {
+// 	fp := path.Join(HTMLSrv.HTMLRoot, "index.html")
+// 	tmpl := template.Must(template.ParseFiles(fp))
+// 	tmpl.Execute(w, nil)
+// 	return
+// }
